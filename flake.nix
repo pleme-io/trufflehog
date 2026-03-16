9f04a7a4b22f1f544fec7e70af63ec428bc6c7d6
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:18 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,23 +10,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "trufflehog";
-        version = "0.0.0-dev";
-        src = self;
-        proxyVendor = true;
-        vendorHash = "sha256-HB3jD0mNjHFyoHmOxxPJ6UdKbVwC5PJ9d2LHJ3DEhXo=";
-        description = "TruffleHog - find leaked credentials and secrets in code repositories";
-        homepage = "https://github.com/pleme-io/trufflehog";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "trufflehog";
+    vendorHash = "sha256-HB3jD0mNjHFyoHmOxxPJ6UdKbVwC5PJ9d2LHJ3DEhXo=";
+    proxyVendor = true;
+    description = "TruffleHog - find leaked credentials and secrets in code repositories";
+    homepage = "https://github.com/pleme-io/trufflehog";
+  };
 }
